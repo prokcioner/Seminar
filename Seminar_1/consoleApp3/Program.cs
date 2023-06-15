1402d8e57379ed909fe67553a0d7cc660beae17b
Artem <artem.krivtsov.96@mail.ru>
2023-06-15 18:11:16 +0300

    consoleApp3 complete

---
+++ Seminar_1/consoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Write("Пожалуйста введите число от 1 до 7 ");
+
+int input = int.Parse(Console.ReadLine());
+
+switch (input)
+{
+  case (1):
+    Console.Write("Первый день недели это понедельник.");
+    break;
+  case (2):
+    Console.Write("Второе день недели это вторник.");
+    break;
+  case (3):
+    Console.Write("Третий день недели это среда.");
+    break;
+  case (4):
+    Console.Write("Четвёртый день недели это четверг.");
+    break;
+  case (5):
+    Console.Write("Пятый день недели это пятница.");
+    break;
+  case (6):
+    Console.Write("Шестой день недели это суббота.");
+    break;
+  case (7):
+    Console.Write("Седьмой день недели это воскресенье.");
+    break;
+  
+  default:
+    Console.Write("Введённое число не равно от 1 до 7.");
+    break;
+}

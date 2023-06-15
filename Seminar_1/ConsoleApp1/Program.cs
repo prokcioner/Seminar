0774c34cb27ae84362619965ddba91b4a07fa22d
Artem <artem.krivtsov.96@mail.ru>
2023-06-15 18:02:06 +0300

    ConsoleApp1 complete

---
+++ Seminar_1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Напишите целое число ");
+int num = int.Parse(Console.ReadLine());
+
+int result = num * num;
+//Знак $ в конструкции ниже позволяет значение reault при обрамлении в фигурные скобки {} вывести
+Console.WriteLine($"Квадрат входного числа: {result}");

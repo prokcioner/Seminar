fa8b6b13e3d378b4e17d3dde3f59d40843365916
Artem <artem.krivtsov.96@mail.ru>
2023-06-18 09:44:46 +0300

    Seminar 2 fix

--- Seminar_2/ConsoleApp2/Program.cs
+++ Seminar_2/ConsoleApp2/Program.cs
@@ -4,5 +4,9 @@ int num = new Random().Next(100,1000);
 Console.WriteLine(num);
 int firstNum = num/100;
 int ThirdNum = num%10;
+
 Console.Write(firstNum);
 Console.WriteLine(ThirdNum);
+
+int result = (firstNum*10) + ThirdNum;
+Console.WriteLine(result);

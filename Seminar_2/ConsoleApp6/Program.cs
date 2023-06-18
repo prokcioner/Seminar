d678be499ec0e806a789a53e18fd005d62c56f2a
Artem <artem.krivtsov.96@mail.ru>
2023-06-18 09:39:41 +0300

    Seminar 2 finish

---
+++ Seminar_2/ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа на вход принимает число (может быть любой размерности). Необходимо вывести reverse этого числа.
+
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int b = a;
+int count = 0;
+
+while (b>0)
+{
+  int d = b%10;
+  int c = b/10;
+  b=c;
+  Console.Write($"{d}");
+  count++;
+}
+
+Console.WriteLine(" ");
+Console.WriteLine($"В числе {a} - {count} цифр");
